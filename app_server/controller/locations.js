7ed2483e6e4512c1194cc6bec40af2e414cb16c1
Rick <ricksanchez@labadudapta.com>
2019-05-19 01:55:08 +0400

    Added locations and location info templates

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,12 +1,12 @@
 /* Get home page */
 module.exports.homeList = function(req, res) {
-  res.render('index', { title: 'Home' });
+  res.render("locations-list", { title: "Home" });
 };
 /* Get location info page */
 module.exports.locationInfo = function(req, res) {
-  res.render('index', { title: 'Location info' });
+  res.render("locations-info", { title: "Location info" });
 };
 /* Get add review page */
 module.exports.addReview = function(req, res) {
-  res.render('index', { title: 'Add review' });
+  res.render("index", { title: "Add review" });
 };
